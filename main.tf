595ed59784ab016aa7bb8df31e6d0ada735454d1
Hector R <birkoff.h@gmail.com>
2018-05-31 11:31:44 +0200

    comment out permissiion
    Commented out, gives issues when deploying to multiple regions

--- main.tf
+++ main.tf
@@ -23,15 +23,15 @@ resource "aws_api_gateway_integration" "api-method-integration" {
 }
 
 # Lambda
-resource "aws_lambda_permission" "apigw-lambda" {
-  statement_id  = "AllowExecutionFromAPIGateway"
-  action        = "lambda:InvokeFunction"
-  function_name = "${var.lambda_fuction_arn}"
-  principal     = "apigateway.amazonaws.com"
+# resource "aws_lambda_permission" "apigw-lambda" {
+#   statement_id  = "AllowExecutionFromAPIGateway"
+#   action        = "lambda:InvokeFunction"
+#   function_name = "${var.lambda_fuction_arn}"
+#   principal     = "apigateway.amazonaws.com"
 
-  # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
-  source_arn = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${var.api_id}/*/${aws_api_gateway_method.api-method.http_method}${var.api_resource_path}"
-}
+#   # More: http://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-control-access-using-iam-policies-to-invoke-api.html
+#   source_arn = "arn:aws:execute-api:${var.region}:${data.aws_caller_identity.current.account_id}:${var.api_id}/*/${aws_api_gateway_method.api-method.http_method}${var.api_resource_path}"
+# }
 
 resource "aws_api_gateway_method_response" "ok" {
   depends_on  = ["aws_api_gateway_method.api-method", "aws_api_gateway_integration.api-method-integration"]
